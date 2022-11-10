27a19fcecbe1849f8f7f8cec2d6122159f629408
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-11-10 11:43:18 -0800

    Yahtzee1

---
+++ Yahtzee.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Dice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Yahtzee
+{
+    private int[] dice = new int[5];
+    private Die6 die;
+    public Yahtzee() {
+        this.roll();
+    }  
+
+    public void roll() {
+        die = new Die6();
+        for (int i = 0; i < 5; i++) {
+            die.roll();
+            this.dice[i] = die.getValue();
+        }
+    }
+    public void roll(int dieNumber) {
+        this.dice[dieNumber - 1] = die.rollAndGetValue();
+    }
+}
+
