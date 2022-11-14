6329f484296a48f67ada7ee81b7cd6afc7ee43cd
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-11-14 11:49:28 -0800

    shamoon

--- Yahtzee.java
+++ Yahtzee.java
@@ -8,6 +8,14 @@ public class Yahtzee
 {
     private int[] dice = new int[5];
     private Die6 die;
+    private String String;
+    private String summer;
+    private int one;
+    private int two;
+    private int three;
+    private int four;
+    private int five;
+    private int six;
     public Yahtzee() {
         this.roll();
     }  
@@ -19,8 +27,46 @@ public class Yahtzee
             this.dice[i] = die.getValue();
         }
     }
+
     public void roll(int dieNumber) {
         this.dice[dieNumber - 1] = die.rollAndGetValue();
     }
-}
 
+    public String toString() {
+        String = "Dice Values: " + dice[0] + " " + dice[1] + " "+ dice[2] + " " + dice[3] + " "+ dice[4];
+        return String;
+    }
+
+    public String summarize() {
+        summer = "";
+        one = 0;
+        two = 0;
+        three = 0;
+        four = 0;
+        five = 0;
+        int a = 0;
+        while (a > 5) {
+            if (dice[a] == 1) {
+                one++;
+            }
+            if (dice[a] == 2) {
+                two++;
+            }
+            if (dice[a] == 3) {
+                three++;
+            }
+            if (dice[a] == 4) {
+                four++;
+            }
+            if (dice[a] == 5) {
+                five++;
+            }
+            else {
+                six++;
+            }
+            a++;
+        }
+        summer = "1-" + one +";2-" + two + ";3-" + three + ";4-" + four + ";5-" + five + ";6-" + six;
+        return summer;
+    }
+}
